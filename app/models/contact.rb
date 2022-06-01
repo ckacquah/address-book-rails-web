6964a04715bd02c862e128c0ee3cff8470407a8f
ckacquah <ckacquah001@gmail.com>
2022-06-01 11:31:31 +0000

    Changing form sizing & Adding phone number validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,9 @@
 class Contact < ApplicationRecord
   include HandleSafely
   include HasSummary
-  
+
+  VALID_PHONE_REGEX = /\+?\(?(([2][3][3])|[0])?\)?[-.\s]?\d{2}[-.\s]?\d{3}[-.\s]?\d{4}?/
+
   attr_accessor :region, :cities, :suburbs, :localities                                             
 
   belongs_to :locality,  -> { active }, class_name: "Locality", foreign_key: "locality_code"
@@ -9,7 +11,7 @@ class Contact < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :phone, presence: true, :numericality => true, :length => { :is => 10 }
   validates :description, presence: true
   validates :locality_code, presence: true
+  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX, message: "must be a valid" }
 end
