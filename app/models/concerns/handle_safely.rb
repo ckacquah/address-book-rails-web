3e9ca05d03c8dccfc3e5788f25c449455280906e
ckacquah <ckacquah001@gmail.com>
2022-06-03 11:18:16 +0000

    Improving UI & Fixing validation issues

--- app/models/concerns/handle_safely.rb
+++ app/models/concerns/handle_safely.rb
@@ -1,9 +1,7 @@
 module HandleSafely
-
   extend ActiveSupport::Concern
 
   included do
-
     self.primary_key = 'assigned_code'
 
     scope :active, -> { where(active_status: true) }
@@ -11,15 +9,12 @@ module HandleSafely
     scope :not_deleted, -> { where(del_status: false) }
 
     scope :active_and_not_deleted, -> { active.not_deleted }
-
   end
 
   class_methods do
-
     def find_safely(id)
       active_and_not_deleted.where(assigned_code: id).order(:updated_at).first
     end
-
   end
 
   def safe_delete
@@ -27,18 +22,25 @@ module HandleSafely
   end
 
   # The function updates the model by making the previous record inactive
+
   # and adding the new update into the database
+
   def safe_update(update_params)
     new_params = {} # new parameters for the new update
+
     attribute_names.each do |attribute_name|
       # Get all the model attributes
+
       symbolic_name = attribute_name.to_sym # Convert the attribute name into a symbol
+
       new_params[symbolic_name] =
         update_params.key?(symbolic_name) ? update_params[symbolic_name] : attributes[attribute_name]
     end
+
     update(active_status: false) # make the old record inactive
-    @new_record = self.class.new(new_params) # Create and insert update
+
+    @new_record = self.class.create(new_params) # Create and insert update
+
     @new_record.save
   end
-
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,11 @@
 class Contact < ApplicationRecord
-
   include HandleSafely
 
   include HasSummary
 
-
-
-  VALID_PHONE_REGEX = /\+?\(?((233)|0)?\)?[-.\s]?\d{2}[-.\s]?\d{3}[-.\s]?\d{4}?/
-
-
+  VALID_PHONE_REGEX = /\+?\(?((233)|0)?\)?[-.\s]?\d{2}[-.\s]?\d{3}[-.\s]?\d{4}?\z/
 
   attr_accessor :region, :cities, :suburbs, :localities
-
-
 
   belongs_to :locality,  -> { active }, class_name: 'Locality', foreign_key: 'locality_code'
 
@@ -20,17 +13,15 @@ class Contact < ApplicationRecord
 
   validates_associated :locality
 
-
-
   validates :first_name, presence: true
 
   validates :last_name, presence: true
 
-  validates :description, presence: true
+  # validates :description, presence: true
 
   validates :locality_code, presence: true
 
-  validates :phone, presence: true, format: { with: VALID_PHONE_REGEX, message: 'must be a valid' }
+  validates :phone, presence: true,
 
+                    format: { with: VALID_PHONE_REGEX, message: 'must be valid. Kindly use the following formats (233xxxxxxxx or 0xxxxxxxxx)' }
 end
-
